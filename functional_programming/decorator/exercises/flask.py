28b41577124a6a3b225f3138591c3fddcf82dd69
Paulo Bueno <paulob.bruno@gmail.com>
2020-02-09 00:27:01 -0300

    Exercise Flask

---
+++ functional_programming/decorator/exercises/flask.py
@@ -0,0 +1,46 @@
+"""
+>>> app = Flask()
+>>> set(app.paths)
+set()
+>>> @app.path('/')
+... def root():
+...     return 'root'
+...
+>>> set(app.paths)
+{'/'}
+>>> root()
+'root'
+>>> app.run('/')
+'root'
+>>> @app.path('/name')
+... def name(user):
+...     return f'User name: {user}'
+...
+>>> list(app.paths)
+['/', '/name']
+>>> name('Python')
+'User name: Python'
+>>> app.run('/name', 'Pro')
+'User name: Pro'
+>>> app.run('/not_a_page',)
+404
+"""
+from decorator import decorator
+
+
+class Flask:
+
+    def __init__(self):
+        self.paths = dict()
+
+    def path(self, path):
+        def decorator(f):
+            self.paths.update({path: f})
+            return f
+
+        return decorator
+
+    def run(self, path, *args, **kwargs):
+        if path not in self.paths:
+            return 404
+        return self.paths[path](*args, **kwargs)
